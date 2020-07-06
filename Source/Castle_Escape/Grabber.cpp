dab92dd07f8ecd2ba6d0453e58d9b48ee73da093
Michael Walker <mwalk762@mtroyal.ca>
2020-07-05 17:50:00 -0700

    Calculate vectors for LineTraceEnd, also add Debug line for ray cast

--- Source/Castle_Escape/Grabber.cpp
+++ Source/Castle_Escape/Grabber.cpp
@@ -1,7 +1,11 @@
 // Copyright Michael S. Walker 2020
 
-
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "Grabber.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -13,22 +17,40 @@ UGrabber::UGrabber()
 	// ...
 }
 
-
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty"));
 }
 
-
-// Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+// Callsed every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation);
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5);
+
+	UE_LOG(LogTemp, Warning, TEXT("Location (%s) Rotation (%s)"),
+		   *PlayerViewPointLocation.ToString(),
+		   *PlayerViewPointRotation.ToString());
+
 	// ...
 }
-
